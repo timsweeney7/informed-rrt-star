b4c3dc51d6cce64271b9055ba312f1289a3e466b
Tim Sweeney <timsweeney97@gmail.com>
2023-04-23 18:29:45 -0400

    updates to rrt_star class

--- rrt_star.py
+++ rrt_star.py
@@ -13,46 +13,52 @@ start_pt = (150, 150)
 pts_explored_set = set()
 nodes_explored = []
 
-#(C2C, index, parent_pt, pt_coor)
-start_node = (0, 0, None, start_pt)
-nodes_explored.append(start_node)
+
+class Node:
+    def __init__(self):
+        self.costToCome = float('inf')
+        self.parentCoordinates = []
+
+    def __str__(self):
+        return f"Cost To Come: {self.costToCome}  Parent Coordinates: {self.parentCoordinates}"
 
 
-def rand_gen ():
-    # generate a random x coordinate within the limit
-    x_coord = random.randint(0, X_MAX)
-    # generate a random y coordinate within the limit
-    y_coord = random.randint(0, Y_MAX)
-
-    rand_pt = (x_coord, y_coord)
-    pts_explored_set.add(rand_pt)
+def create_node_info_map(color_map):
+    node_info_map = np.ndarray.tolist(color_map)
     
-    return(rand_pt)
+    node_info_map[:][:] = {"c2c": 10, "parentCoor": (10,10)}
+    return node_info_map
+
+
+def get_random_point ():
+    # generate a random x coordinate within the limit
+    x_coord = random.randint(0, mapping.X_MAX_SCALED)
+    # generate a random y coordinate within the limit
+    y_coord = random.randint(0, mapping.Y_MAX_SCALED)
+    rand_pt = (x_coord, y_coord)
+    return rand_pt
+    
 
 def distance (pt1, pt2): 
     distance = round(math.sqrt(pow(pt2[0] - pt1[0], 2) + pow(pt2[1] - pt1[1], 2)))
     return distance
 
-def create_map (): 
-    global index
-    index += 1
-    new_pt = rand_gen ()
-    dist = distance(start_pt, new_pt)
-    new_node = (dist, index, start_pt, new_pt)
-    nodes_explored.append(new_node)
-    return nodes_explored
 
-for i in range (0,10): 
-    create_map()
+def explore (color_map): 
+    new_pt = get_random_point()
+    if mapping.point_is_valid(color_map=color_map, coordinates=new_pt):
+        c2c = distance(start_pt, new_pt)
+        parentCoor = [0,0]
+        new_node = Node()
+        new_node.costToCome = c2c
+        new_node.parentCoordinates = parentCoor
+        nodes_explored.append(new_node)
+        return nodes_explored
+
 
 if __name__ == "__main__":
-    # print("obs_pts:", obstacle_points)
+
     color_map = mapping.draw_simple_map()
-    cv.imshow('Informed RRT* Algorith', color_map)
-    cv.waitKey(0)
-
-
-print("explored nodes:", nodes_explored)
-# x=rand_gen()
-# # print(pts_explored)
-# print(x)
+    node_info_map = create_node_info_map(color_map)
+    #cv.imshow('Informed RRT* Algorithm', color_map)
+    #cv.waitKey(0)

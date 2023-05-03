2c2c6be7c52b7f938903e6d5ef3969bdf08bdaaf
anarmouq <amr.narmouq@gmail.com>
2023-05-02 23:39:27 -0400

    updated ellipse 2 test code

--- ellipse_test_2.py
+++ ellipse_test_2.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
-import matplotlib.path as mpath
 import numpy as np
 
-def rotated_ellipse(center_x, center_y, point1, point2, semi_major_axis, semi_minor_axis):
+def rotated_ellipse(center_x, center_y, point1, point2, semi_major_axis, semi_minor_axis, angle):
+    # Rotate the points
+    cos_angle = np.cos(angle)
+    sin_angle = np.sin(angle)
+    rot_matrix = np.array([[cos_angle, -sin_angle], [sin_angle, cos_angle]])
+    point1 = np.dot(rot_matrix, point1)
+    point2 = np.dot(rot_matrix, point2)
+
     # Calculate the semi-major and semi-minor axes of the ellipse
     distance = np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
 
-    # Calculate the angle of rotation of the ellipse
-    angle = np.arctan2(point2[1] - point1[1], point2[0] - point1[0])
+    # # Calculate the angle of rotation of the ellipse
+    # angle = np.arctan2(point2[1] - point1[1], point2[0] - point1[0])
 
     # Generate coordinates of the rotated ellipse
     theta = np.linspace(0, 2*np.pi, 1000)
@@ -16,35 +22,35 @@ def rotated_ellipse(center_x, center_y, point1, point2, semi_major_axis, semi_mi
 
     return x_rotated, y_rotated
 
-# Define the ellipse parameters
-point1 = [125, 125]
-point2 = [135, 125]
+# Example usage
+point1 = np.array([125, 125])
+point2 = np.array([135, 125])
 center_x = (point1[0] + point2[0]) / 2
 center_y = (point1[1] + point2[1]) / 2
 semi_major_axis = 50
 semi_minor_axis = 25
+angle = np.arctan2(point2[1] - point1[1], point2[0] - point1[0])
 
-# Generate the ellipse
-x, y = rotated_ellipse(center_x, center_y, point1, point2, semi_major_axis, semi_minor_axis)
+x, y = rotated_ellipse(center_x, center_y, point1, point2, semi_major_axis, semi_minor_axis, angle)
 
-# Create a Path object for the ellipse
-ellipse_path = mpath.Path(np.column_stack((x, y)))
-
-# Generate random points within the ellipse
-n_points = 100
-points = []
-while len(points) < n_points:
-    # Generate random x and y coordinates within the range of the ellipse
-    x_rand = np.random.uniform(center_x - semi_major_axis, center_x + semi_major_axis)
-    y_rand = np.random.uniform(center_y - semi_minor_axis, center_y + semi_minor_axis)
-    
-    # Check if the point is inside the ellipse
-    if ellipse_path.contains_point([x_rand, y_rand]):
-        points.append([x_rand, y_rand])
-
-# Plot the ellipse and the random points
+# Plot the ellipse
 fig, ax = plt.subplots()
 ax.plot(x, y)
-ax.scatter(np.array(points)[:, 0], np.array(points)[:, 1])
 ax.axis('equal')
+
+# Generate random points within the rotated ellipse
+num_points = 1000
+theta = 2 * np.pi * np.random.rand(num_points)
+radius = np.sqrt(np.random.rand(num_points))
+# Define the rotation matrix for the ellipse
+cos_angle = np.cos(angle)
+sin_angle = np.sin(angle)
+rot_matrix = np.array([[cos_angle, -sin_angle], [sin_angle, cos_angle]])
+# Rotate the random points using the same rotation matrix
+rand_points = np.dot(rot_matrix, np.array([semi_major_axis * radius * np.cos(theta), semi_minor_axis * radius * np.sin(theta)]))
+x_points = rand_points[0] + center_x
+y_points = rand_points[1] + center_y
+
+# Plot the random points
+ax.scatter(x_points, y_points, s=5, color='red')
 plt.show()
